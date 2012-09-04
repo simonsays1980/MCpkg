2c667d5a7e1b618adc5ba8114b0071b85a8b4826
simon.zehnder <simon.zehnder@gmail.com>
2012-09-04 10:49:58 +0200

    main.cpp created

---
+++ MCpkg/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * main.cc
+ *
+ *  Created on: Sep 3, 2012
+ *      Author: simon
+ */
+
+#ifdef INSIDE
+
+#include <Rcpp.h>
+#include <RInside.h>                    // for the embedded R via RInside
+#include "rcpp_hello_world.h"
+
+using namespace Rcpp;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	RInside R(argc, argv); // create an embedded R instance
+	SEXP s = rcpp_hello_world();
+	Language call("print", s);
+	call.eval();
+	return 0;
+}
+
+#endif
